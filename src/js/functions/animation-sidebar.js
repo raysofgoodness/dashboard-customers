16da4f4266626af6c1f8053b0f4959c84c5ad881
raysofgoodness <shady5324@gmail.com>
2022-10-24 00:08:09 +0300

    fixed animation sidebar and media main queries

--- src/js/functions/animation-sidebar.js
+++ src/js/functions/animation-sidebar.js
@@ -1,27 +1,28 @@
 import { gsap } from "gsap";
 
 export const animationSidebar = () => {
-  const wrapperBurger = document.querySelector('.burger');
-  const burger = document.querySelector('#burger');
-  const burgerTl = gsap.timeline();
+  const burger = document.querySelector('.burger');
+  const sidebar = document.querySelector('.sidebar');
+  const tl = gsap.timeline();
 
-  burgerTl.pause();
-  burgerTl.to("#top-stroke", 0.5, {y: 10});
-  burgerTl.to("#bot-stroke", 0.5, {y: -10}, "-=0.5");
-  burgerTl.to("#burger", 1, {rotation: -360}, "-=0.5");
-  burgerTl.to('#top-stroke', 0.5, {rotation: -45, transformOrigin:"50% 50%"}, "-=0.5" );
-  burgerTl.to('#bot-stroke', 0.5, {rotation: 45, transformOrigin:"50% 50%"}, "-=1");
-  burgerTl.to('#mid-stroke', 0.5, {rotation: 45, transformOrigin:"50% 50%"}, "-=1" );
+  tl.pause();
+  tl.to("#top-stroke", 0.5, {y: 10});
+  tl.to("#bot-stroke", 0.5, {y: -10}, "-=0.5");
+  tl.to("#burger", 1, {rotation: -360}, "-=0.5");
+  tl.to('#top-stroke', 0.5, {rotation: -45, transformOrigin:"50% 50%"}, "-=0.5" );
+  tl.to('#bot-stroke', 0.5, {rotation: 45, transformOrigin:"50% 50%"}, "-=1");
+  tl.to('#mid-stroke', 0.5, {rotation: 45, transformOrigin:"50% 50%"}, "-=1" );
 
   let clicked = false;
 
-  wrapperBurger.addEventListener('click', () => {
+  burger.addEventListener('click', () => {
+    sidebar.classList.toggle('sidebar--active');
 
     if (!clicked) {
-      burgerTl.play();
+      tl.play();
       clicked = true;
     } else {
-      burgerTl.reverse();
+      tl.reverse();
       clicked = false;
     }
   });
